c5d79fa0bf89a3c0ea31357ce3073e9a73a57853
tharaka <tharakakushan@gmail.com>
2020-05-30 06:27:33 +0530

    Add specific ids for the main activity  input TextViews

--- app/src/main/java/com/example/android/travelguideme/MainActivity.java
+++ app/src/main/java/com/example/android/travelguideme/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.travelguideme;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView selectCountry = (TextView) findViewById(R.id.)
     }
 }
