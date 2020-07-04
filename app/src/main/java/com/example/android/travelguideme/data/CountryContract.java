62252820a632ab039c37e0082661bb696e52e955
Kushan Tharaka <tharakakushan@gmail.com>
2020-07-04 23:17:02 +0530

    Added Insert data class to insert created data -all the data those we are generated by our own

--- app/src/main/java/com/example/android/travelguideme/data/CountryContract.java
+++ app/src/main/java/com/example/android/travelguideme/data/CountryContract.java
@@ -2,22 +2,26 @@ package com.example.android.travelguideme.data;
 
 import android.provider.BaseColumns;
 
-public class CountryContract implements BaseColumns {
-    //The main table name of the  App
-    public static final String TABLE_NAME = "countries";
+public class CountryContract  {
+    public static abstract class InsertCountryData implements BaseColumns{
 
-    //auto incrementing id
-    public static final String COLUMN_COUNTRY_ID = BaseColumns._ID;
+        //The main table name of the  App
+        public static final String TABLE_NAME = "countries";
 
-    public static final String COLUMN_COUNTRY_NAME = "country";
+        //auto incrementing id
+        public static final String COLUMN_COUNTRY_ID = BaseColumns._ID;
 
-    public static final String COLUMN_COUNTRY_CURRENCY = "currency";
+        public static final String COLUMN_COUNTRY_NAME = "country";
 
-    public static final String COLUMN_COUNTRY_LANGUAGE = "language";
+        public static final String COLUMN_COUNTRY_CURRENCY = "currency";
 
-    public static final String COLUMN_COUNTRY_CAPITAL = "capital";
+        public static final String COLUMN_COUNTRY_LANGUAGE = "language";
 
-    public static final String COLUMN_COUNTRY_CITIES = "cities";
+        public static final String COLUMN_COUNTRY_CAPITAL = "capital";
 
-    public static final String COLUMN_COUNTRY_POPULATION = "population";
+        public static final String COLUMN_COUNTRY_CITIES = "cities";
+
+        public static final String COLUMN_COUNTRY_POPULATION = "population";
+    }
+
 }
--- app/src/main/java/com/example/android/travelguideme/data/CountryDbHelper.java
+++ app/src/main/java/com/example/android/travelguideme/data/CountryDbHelper.java
@@ -1,4 +1,30 @@
 package com.example.android.travelguideme.data;
 
-public class CountryDbHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import com.example.android.travelguideme.data.CountryContract.InsertCountryData;
+import androidx.annotation.Nullable;
+
+public class CountryDbHelper extends SQLiteOpenHelper{
+
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "guide.db";
+
+
+    public CountryDbHelper(Context context) {
+        super(context, DATABASE_NAME,null,  DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //creating database
+        String SQL_CREATE_COUNTRY_TABLE = "CREATE TABLE"+ InsertCountryData.TABLE_NAME;
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
