273fa9dd6fe81eb27d41f4fcd3e35907ecd91517
Kushan Tharaka <tharakakushan@gmail.com>
2020-07-04 22:15:45 +0530

    Added attributes to contract

--- app/src/main/java/com/example/android/travelguideme/data/CountryContract.java
+++ app/src/main/java/com/example/android/travelguideme/data/CountryContract.java
@@ -1,4 +1,23 @@
 package com.example.android.travelguideme.data;
 
-public class CountryContract {
+import android.provider.BaseColumns;
+
+public class CountryContract implements BaseColumns {
+    //The main table name of the  App
+    public static final String TABLE_NAME = "countries";
+
+    //auto incrementing id
+    public static final String COLUMN_COUNTRY_ID = BaseColumns._ID;
+
+    public static final String COLUMN_COUNTRY_NAME = "country";
+
+    public static final String COLUMN_COUNTRY_CURRENCY = "currency";
+
+    public static final String COLUMN_COUNTRY_LANGUAGE = "language";
+
+    public static final String COLUMN_COUNTRY_CAPITAL = "capital";
+
+    public static final String COLUMN_COUNTRY_CITIES = "cities";
+
+    public static final String COLUMN_COUNTRY_POPULATION = "population";
 }
