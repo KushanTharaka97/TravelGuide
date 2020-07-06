2da5d147af984ba053d9ba83744b53280f7e4cdd
Kushan Tharaka <tharakakushan@gmail.com>
2020-07-06 20:49:02 +0530

    Created test display in Your memo activity

--- app/src/main/java/com/example/android/travelguideme/YourMemo.java
+++ app/src/main/java/com/example/android/travelguideme/YourMemo.java
@@ -3,12 +3,20 @@ package com.example.android.travelguideme;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
+import com.example.android.travelguideme.data.CountryContract;
+import com.example.android.travelguideme.data.CountryDbHelper;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.example.android.travelguideme.data.CountryContract.InsertCountryData;
 
 public class YourMemo extends AppCompatActivity {
+
+    private CountryDbHelper mDbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +32,28 @@ public class YourMemo extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    mDbHelper = new CountryDbHelper(this);
+
+
+    }
+
+
+    private void displayDatabaseInfo() {
+        // Create and/or open a database to read from it
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+
+        // Perform this raw SQL query "SELECT * FROM pets"
+        // to get a Cursor that contains all rows from the pets table.
+        Cursor cursor = db.rawQuery("SELECT * FROM " + InsertCountryData.TABLE_NAME, null);
+        try {
+            // Display the number of rows in the Cursor (which reflects the number of rows in the
+            // pets table in the database).
+            TextView displayView = (TextView) findViewById(R.id.viewTest);
+            displayView.setText("Number of rows in pets database table: " + cursor.getCount());
+        } finally {
+            // Always close the cursor when you're done reading from it. This releases all its
+            // resources and makes it invalid.
+            cursor.close();
+        }
     }
 }
