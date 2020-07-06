0c55a2ee57ec73a0977155c423f9a9297d281ff1
Kushan Tharaka <tharakakushan@gmail.com>
2020-07-06 17:43:55 +0530

    ADDED FLOATING action Button to the aboutUs and set up in the activity also

--- app/src/main/java/com/example/android/travelguideme/AboutUs.java
+++ app/src/main/java/com/example/android/travelguideme/AboutUs.java
@@ -2,7 +2,11 @@ package com.example.android.travelguideme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class AboutUs extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class AboutUs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+
+        // Setup FAB to open EditorActivity
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AboutUs.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
