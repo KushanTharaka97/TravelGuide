6fd079b30bcb0c2958a71d50cf93c38f671b6d5b
Kushan Tharaka <tharakakushan@gmail.com>
2020-07-06 17:47:31 +0530

    ADDED FLOATING action Button to the YourMemo view and set up in the activity also

--- app/src/main/java/com/example/android/travelguideme/YourMemo.java
+++ app/src/main/java/com/example/android/travelguideme/YourMemo.java
@@ -2,7 +2,11 @@ package com.example.android.travelguideme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class YourMemo extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class YourMemo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_your_memo);
+
+        // Setup FAB to open EditorActivity
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(YourMemo.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
