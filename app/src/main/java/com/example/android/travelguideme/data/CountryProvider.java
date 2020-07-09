c736cb96cbf8f4a3713c4ff93c6a1617b8a1edbc
Kushan Tharaka <tharakakushan@gmail.com>
2020-07-09 11:41:26 +0530

    SET static values in CountryProvider class

--- app/src/main/java/com/example/android/travelguideme/data/CountryProvider.java
+++ app/src/main/java/com/example/android/travelguideme/data/CountryProvider.java
@@ -2,6 +2,7 @@ package com.example.android.travelguideme.data;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 
@@ -9,6 +10,33 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 public class CountryProvider extends ContentProvider {
+    /** Tag for the log messages */
+    public static final String LOG_TAG = CountryProvider.class.getSimpleName();
+
+    /** URI matcher code for the content URI for the country table */
+    private static final int COUNTRIES = 100;
+
+    /** URI matcher code for the content URI for a single country in country table */
+    private static final int COUNTRY_ID = 101;
+
+    /**
+     * UriMatcher object to match a content URI to a corresponding code.
+     * The input passed into the constructor represents the code to return for the root URI.
+     * It's common to use NO_MATCH as the input for this case.
+     */
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    // Static initializer. This is run the first time anything is called from this class.
+    static {
+        // The calls to addURI() go here, for all of the content URI patterns that the provider
+        // should recognize. All paths added to the UriMatcher have a corresponding code to return
+        // when a match is found.
+
+        sUriMatcher.addURI(CountryContract.CONTENT_AUTHORITY, CountryContract.PATH_COUNTRIES, COUNTRIES);
+        sUriMatcher.addURI(CountryContract.CONTENT_AUTHORITY,CountryContract.PATH_COUNTRIES+ "/#" ,COUNTRY_ID);
+    }
+
+
     @Override
     public boolean onCreate() {
         return false;
