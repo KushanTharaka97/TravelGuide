ed75c8e12b0b5fb3df1f5d4b2f9025850c0067a3
Kushan Tharaka <tharakakushan@gmail.com>
2020-07-09 19:00:50 +0530

    Projection Created

--- app/src/main/java/com/example/android/travelguideme/SelectCountry.java
+++ app/src/main/java/com/example/android/travelguideme/SelectCountry.java
@@ -3,12 +3,40 @@ package com.example.android.travelguideme;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.android.travelguideme.data.CountryContract.InsertCountryData;
+import com.example.android.travelguideme.data.CountryDbHelper;
 
 public class SelectCountry extends AppCompatActivity {
+    private CountryDbHelper mDbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_country);
+
+        mDbHelper = new CountryDbHelper(this);
+    }
+
+    //method implementing for display all the countries in this window
+    private void displayDatabaseInfo(){
+        // To access our database, we instantiate our subclass of SQLiteOpenHelper
+        // and pass the context, which is the current activity.
+
+        //display in this space
+        TextView displayView = findViewById(R.id.text_view_country);
+
+        //using Uri method to call the database
+        String[] projection = {
+                InsertCountryData.COLUMN_COUNTRY_NAME,
+                InsertCountryData.COLUMN_COUNTRY_CURRENCY,
+                InsertCountryData.COLUMN_COUNTRY_POPULATION,
+                InsertCountryData.COLUMN_COUNTRY_LANGUAGE,
+                InsertCountryData.COLUMN_COUNTRY_CAPITAL,
+                InsertCountryData.COLUMN_COUNTRY_CITY_01,
+                InsertCountryData.COLUMN_COUNTRY_CITY_02
+        };
+
     }
 }
